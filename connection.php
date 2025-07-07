cba4ac43c4bfd46744d0368759431f79997211e2
Jenish776 <kotadiyajenish03@gmail.com>
2025-07-07 22:40:21 +0530

    added env clever cloud

--- connection.php
+++ connection.php
@@ -1,9 +1,8 @@
-<?php
+<!-- <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
-
 
 $obj = new mysqli("sql203.infinityfree.com","if0_39412766","PkMJhMsdvRN","if0_39412766_blogsDB");
 if($obj->connect_errno != 0)
@@ -13,4 +12,26 @@ if($obj->connect_errno != 0)
 }
 echo json_encode(["message" => "Hello from backend"]);
 
+?> -->
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
+
+// Clever Cloud environment variables for a MySQL addon
+$host = getenv("MYSQL_ADDON_HOST");
+$user = getenv("MYSQL_ADDON_USER");
+$password = getenv("MYSQL_ADDON_PASSWORD");
+$database = getenv("MYSQL_ADDON_DB");
+
+// Connect using environment variables
+$obj = new mysqli($host, $user, $password, $database);
+
+if ($obj->connect_errno != 0) {
+    echo json_encode(["error" => $obj->connect_error]);
+    exit;
+}
+
+echo json_encode(["message" => "Hello from backend"]);
 ?>
